a9f718106fee35939e82aad65fd3e3479626fdfb
Michael <michael@Admins-MacBook-Pro.local>
2021-10-02 14:02:34 -0400

    got the datetime attributes of events

--- backend/readcal.py
+++ backend/readcal.py
@@ -9,6 +9,7 @@ gcal = Calendar.from_ical(g.read())
 for component in gcal.walk():
     if component.name == "VEVENT":
         print(component.get('summary'))
-        # print(component.get('dtend'))
-        # print(component.get('dtstamp'))
+        print(component.get('dtstart').dt)
+        print(component.get('dtend').dt)
+        print(component.get('dtstamp').dt)
 g.close()
