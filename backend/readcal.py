c048cd07f76470c21242d7ecbdedb7b10002f1bb
Michael <michael@Admins-MacBook-Pro.local>
2021-10-02 14:22:06 -0400

    basic DB and also modified the calendar reader to read from input

--- backend/readcal.py
+++ backend/readcal.py
@@ -4,7 +4,10 @@ from icalendar import Calendar, Event
 from icalendar import vDatetime
 from datetime import datetime
 
-g = open('mhzhou@andrew.cmu.edu.ics','rb')
+
+calName = input("Put the name of your calendar file here:\n")
+
+g = open(calName,'rb')
 gcal = Calendar.from_ical(g.read())
 for component in gcal.walk():
     if component.name == "VEVENT":
@@ -13,3 +16,22 @@ for component in gcal.walk():
         print(component.get('dtend').dt)
         print(component.get('dtstamp').dt)
 g.close()
+
+
+def setUpDatabase():
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/calendar.db')
+    cur = conn.cursor()
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS Calendars 
+    (id INTEGER PRIMARY KEY, 
+    deck TEXT, 
+    front TEXT UNIQUE, 
+    back TEXT)''')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS Users 
+    (id INTEGER PRIMARY KEY, name TEXT UNIQUE)''')
+
+    conn.commit()
+    
+    return cur, conn
